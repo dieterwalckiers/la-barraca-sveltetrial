8cd54bb0f9980f7aa4343e4846d4dde96c5c1377
dyte <dieter.walckiers@gmail.com>
2020-05-21 12:19:57 +0200

    debugging

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,12 +1,14 @@
 import sanityClient from "@sanity/client";
 import { getConfig } from "./config";
 
+
+console.log("using dataset", getConfig().sanity.dataset);
+
 const client = sanityClient({
   projectId: "p3ezynln",
-  dataset: "development",
+  dataset: getConfig().sanity.dataset,
   useCdn: true
 });
 
-console.log("using dataset development");
 
 export default client;
